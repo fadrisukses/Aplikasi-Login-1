e2ca723926f376c18acdf629dfed7728bb2f7ab8
fadrisukses <125943710+fadrisukses@users.noreply.github.com>
2023-02-21 12:24:06 +0700

    Update login-script.js
    mengubah comentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,18 +5,25 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+//membuat variabel untuk setiap element view
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+//membuat variabel untuk menyimpan informasi email dan password yang valid
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
-
+//mendapatkan input email dan password pengguna dari form
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+//logika kondisi perbandingan dengan:
+  //jika email dan password benar maka akan memasuki halaman beranda dengan fungsi goToHome
+  //jika email dan password salah maka akan muncul pop up dengan menggunakan fungsi showPupUp
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
   } else {
