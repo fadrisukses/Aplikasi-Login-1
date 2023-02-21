4e65e1c2104890cc5a33a3cd80337643f41b3cae
fadrisukses <125943710+fadrisukses@users.noreply.github.com>
2023-02-21 12:27:55 +0700

    Update login-script.js
    revisi comment pada login js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,32 +1,30 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
-//membuat variabel untuk setiap element view
+/* Comment : Membuat variabel untuk setiap elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
-
-//membuat variabel untuk menyimpan informasi email dan password yang valid
+ 
+/* Comment : Membuat variabel untuk menyimpan informasi email dan password. */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
-
-
+ 
+/* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
-//mendapatkan input email dan password pengguna dari form
+  
+  /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
-
-//logika kondisi perbandingan dengan:
-  //jika email dan password benar maka akan memasuki halaman beranda dengan fungsi goToHome
-  //jika email dan password salah maka akan muncul pop up dengan menggunakan fungsi showPupUp
+ 
+  /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
+ 
+    /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
+ 
   } else {
+ 
+    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
+ 
   }
 });
